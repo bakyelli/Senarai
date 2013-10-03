c76b577d856236f7cdca00fba03e82738e68ecbe
Joe Burgess <joe@flatironschool.com>
2013-10-03 10:52:54 -0400

    Created Single View Project

---
+++ Senarai/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Senarai
+//
+//  Created by Joe Burgess on 10/3/13.
+//  Copyright (c) 2013 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Senarai/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Senarai
+//
+//  Created by Joe Burgess on 10/3/13.
+//  Copyright (c) 2013 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
