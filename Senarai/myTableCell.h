b1a949fa1c5b6d8ab8d9867eabf64ff121d24f87
Joe Burgess <joe@flatironschool.com>
2013-10-16 08:17:07 -0400

    Adding in MyTableCell

---
+++ Senarai/myTableCell.h
@@ -0,0 +1,13 @@
+//
+//  myTableCell.h
+//  Senarai
+//
+//  Created by Joe Burgess on 10/15/13.
+//  Copyright (c) 2013 Joe Burgess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface myTableCell : NSObject
+
+@end
---
+++ Senarai/myTableCell.m
@@ -0,0 +1,13 @@
+//
+//  myTableCell.m
+//  Senarai
+//
+//  Created by Joe Burgess on 10/15/13.
+//  Copyright (c) 2013 Joe Burgess. All rights reserved.
+//
+
+#import "myTableCell.h"
+
+@implementation myTableCell
+
+@end
